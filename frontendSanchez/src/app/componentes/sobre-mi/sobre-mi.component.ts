2a6fd5306947ba0d50440189069e174e2e73839c
jaimesvlucas <jaimesanchez456@gmail.com>
2021-06-06 20:12:28 +0200

    Commit 06-06-21 Cambio en el mapa

--- frontendSanchez/src/app/componentes/sobre-mi/sobre-mi.component.ts
+++ frontendSanchez/src/app/componentes/sobre-mi/sobre-mi.component.ts
@@ -14,7 +14,7 @@ export class SobreMiComponent implements OnInit {
 
   ngOnInit(): void {
     this.mapa = L.map('posicionMapa').setView([39.383758746966464, -3.2097712749918466], 16)
-    L.marker([39.383758746966464, -3.2097712749918466]).addTo(this.mapa).bindPopup('ToWatchList SL').openPopup();
+    L.marker([39.383758746966464, -3.2097712749918466]).addTo(this.mapa).bindPopup('ToWatchList SL');
     const trozos = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
       maxZoom: 19
     })
